bd43f41091be217db458a2f19b54a1ae68e2ace9
Dandadan11 <swatplay666@yandex.ru>
2025-11-21 22:49:57 +0300

    Улучшение дизайна: добавлена навигация, карточки анкет, адаптивный дизайн

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Profile
 
 def home(request):
-    profiles = Profile.objects.all().order_by('-created_at')[:10]  # 10 последних анкет
+    profiles = Profile.objects.all().order_by('-created_at')  # Сортировка по дате
     return render(request, 'core/home.html', {'profiles': profiles})
