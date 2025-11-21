4411f12941ca8bb95fbb01e510ff124a89a829f4
Dandadan11 <swatplay666@yandex.ru>
2025-11-21 22:37:24 +0300

    Complete Day 1: Working Django project with admin, models, and templates

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Game, Profile
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'game', 'rank', 'created_at']
+    list_filter = ['game', 'rank']
+    search_fields = ['title', 'user__username']
